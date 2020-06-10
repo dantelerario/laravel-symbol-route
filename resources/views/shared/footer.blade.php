364d648b0e4dbb0d7f5fff772cdd115a76685a76
Dante L'Erario <dantelerario@gmail.com>
2020-06-10 16:42:06 +0200

    done

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <ul>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Help</a></li>
+    </ul>
+</footer>
---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Language</a></li>
+    </ul>
+</header>
